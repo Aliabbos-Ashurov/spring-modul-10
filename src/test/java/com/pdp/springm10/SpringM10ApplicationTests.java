e5a78518159b01ada1d7e3b1d83710062e38fe2f
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2024-09-12 14:15:47 +0500

    ...

---
+++ src/test/java/com/pdp/springm10/SpringM10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdp.springm10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringM10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
